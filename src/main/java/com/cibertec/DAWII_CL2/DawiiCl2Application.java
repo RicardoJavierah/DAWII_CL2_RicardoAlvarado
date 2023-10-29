7ac4e4094da9b706ff1f140a56d3b4248e9fe86b
ricardojavierah <ricardojavierah@gmail.com>
2023-10-28 19:55:17 -0500

    avance01

---
+++ src/main/java/com/cibertec/DAWII_CL2/DawiiCl2Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAWII_CL2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/DAWII_CL2/DawiiCl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAWII_CL2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
